9d563ab3b3982013f7fc9a069919e55d959680b2
Celebrimbor1208 <114787923+Celebrimbor1208@users.noreply.github.com>
2024-07-07 09:03:13 -0500

    Create Fingon.py
    Well, I really want to know how  I am developing in this exercise. if there is a fail, I hope . We comment me .

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Fingon.py
@@ -0,0 +1,34 @@
+#Crea un comentario en el código y coloca la URL del sitio web oficial del leguaje de programación que has elegido
+#URL:https://www.python.org/
+
+'''
+Representa las diferentes sintaxis que existen 
+de crear comentarios
+'''
+#Bloque
+'''
+frase
+al azar
+'''
+"""
+frase al azar
+"""
+
+#Crea una variable(y una constante si el lenguaje lo soporta).
+x=5
+print("La variable constante es :"),x
+
+#Crea variables representando todos los tipos de datos primitivos
+#Variables númericas,cadena de caracteres, booleanos
+
+num1=3.14
+texto='Hello there'
+booleano=True
+especial=None
+
+print(num1)
+print(texto)
+print(booleano)
+print(especial)
+
+print("Hola,Python")
